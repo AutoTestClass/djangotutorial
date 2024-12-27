8bb6fb40f68b14e645da8cc7a593b4b9bad8faf6
defnngj <defnngj@gmail.com>
2024-12-27 20:58:39 +0800

    feat: setting ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*+e478-v!yyu5gmvip2h1w@k&%j%y38q=n@y!&8qz!&w7iem@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INTERNAL_IPS = ['127.0.0.1']
 
